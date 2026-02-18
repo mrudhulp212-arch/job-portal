4c486aa23aec7b29929196cddb6ab030204318ee
Mrudhul P <mrudhulp212@gmail.com>
2026-02-18 11:55:44 +0530

    a message added in index.js

--- Job_Board_Platform-Backend/index.js
+++ Job_Board_Platform-Backend/index.js
@@ -52,3 +52,7 @@ app.use((err, req, res, next) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 })
+
+app.get('/', (req, res) => {
+  res.send('Server started')
+})
